1a946ea3d4591f401caca388c7fbdee6811fa916
kennedyaquino <kennedy.eletronica.94@gmail.com>
2024-06-19 23:32:04 -0300

    feat: add repository and service

--- src/main/java/com/br/orderms/listener/OrderCreatedListener.java
+++ src/main/java/com/br/orderms/listener/OrderCreatedListener.java
@@ -1,9 +1,11 @@
 package com.br.orderms.listener;
 
 import com.br.orderms.listener.dto.OrderCreatedEvent;
+import com.br.orderms.service.OrderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +15,18 @@ import static com.br.orderms.config.RabbitMqConfig.ORDER_CREATED_QUEUE;
 public class OrderCreatedListener {
 
     private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderCreatedListener(OrderService orderService) {
+        this.orderService = orderService;
+    }
 
     @RabbitListener(queues = ORDER_CREATED_QUEUE)
     public void listen(Message<OrderCreatedEvent> message) {
-
         logger.info("Message consumed: {}", message);
+
+        orderService.save(message.getPayload());
 
     }
 }
---
+++ src/main/java/com/br/orderms/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.br.orderms.service;
+
+import com.br.orderms.entity.OrderEntity;
+import com.br.orderms.entity.OrderItem;
+import com.br.orderms.listener.dto.OrderCreatedEvent;
+import com.br.orderms.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public void save(OrderCreatedEvent event) {
+        var entity = new OrderEntity();
+
+        entity.setOrderId(event.codigoPedido());
+        entity.setCustomerId(event.codigoCliente());
+        entity.setItems(getOrderItems(event));
+        entity.setTotal(getTotal(event));
+
+        orderRepository.save(entity);
+    }
+
+    private BigDecimal getTotal(OrderCreatedEvent event) {
+        return event.itens().stream().map(i -> i.preco().multiply(BigDecimal.valueOf(i.quantidade()))).reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+    }
+
+    private static List<OrderItem> getOrderItems(OrderCreatedEvent event) {
+        return event.itens().stream().map(i -> new OrderItem(i.produto(), i.quantidade(), i.preco())).toList();
+    }
+}
