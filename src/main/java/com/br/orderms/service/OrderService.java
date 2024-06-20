e64bc7cde0b7fe320439d347cbbefa4db534bcef
kennedyaquino <kennedy.eletronica.94@gmail.com>
2024-06-20 00:07:57 -0300

    feat: implement total orders

--- src/main/java/com/br/orderms/service/OrderService.java
+++ src/main/java/com/br/orderms/service/OrderService.java
@@ -5,22 +5,29 @@ import com.br.orderms.entity.OrderEntity;
 import com.br.orderms.entity.OrderItem;
 import com.br.orderms.listener.dto.OrderCreatedEvent;
 import com.br.orderms.repository.OrderRepository;
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final MongoTemplate mongoTemplate;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, MongoTemplate mongoTemplate) {
         this.orderRepository = orderRepository;
+        this.mongoTemplate = mongoTemplate;
     }
 
     public void save(OrderCreatedEvent event) {
@@ -38,6 +45,17 @@ public class OrderService {
         var orders = orderRepository.findAllByCustomerId(customerId, pageRequest);
 
         return orders.map(OrderResponse::fromEntity);
+    }
+
+    public BigDecimal findTotalOnOrdersByCustomerId(Long customerId) {
+        var aggregations = newAggregation(
+                match(Criteria.where("customerId").is(customerId)),
+                group().sum("total").as("total")
+        );
+
+        var response = mongoTemplate.aggregate(aggregations, "tb_orders", Document.class);
+
+        return  new BigDecimal(response.getUniqueMappedResult().get("total").toString());
     }
 
     private BigDecimal getTotal(OrderCreatedEvent event) {
